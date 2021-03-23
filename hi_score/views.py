0153625dfcfaec074c32da985e8234b7e48365ba
Darren Fielding <2424957f@student.gla.ac.uk>
2021-03-23 22:22:45 +0000

    Clicking on a genre now shows games of that genre

--- hi_score/views.py
+++ hi_score/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 from hi_score.models import Genre
 from hi_score.models import Game
 from hi_score.models import Review
