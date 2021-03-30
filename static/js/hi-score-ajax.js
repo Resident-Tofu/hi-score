522bc72cf7ae6996ef07fe42e1494829b878ca00
Darren Fielding <2424957f@student.gla.ac.uk>
2021-03-30 17:45:49 +0100

    Now displays number of stars for review rating

--- static/js/hi-score-ajax.js
+++ static/js/hi-score-ajax.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+    
     $(".stars").each(function() {
-        $(this).text($(this).data("rat"));
+        var str = "";
+        var num = $(this).data("rat");
+        for (i = 0; i < num; i += 1) { str += "&#11088;"; }
+        $(this).html(str);
     });
+
+    /*
     $(".cap").each(function() {
         $(this).text($(this).data("emb"));
     });
+    */
+
 });
