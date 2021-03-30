5dd41c049fa34cca881a3eb64f83c0000991a2d9
Aidan Dow <aidan@Aidans-MacBook-Pro.local>
2021-03-30 16:54:49 +0100

    Modified Myaccount page

--- hiscore_project/urls.py
+++ hiscore_project/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from hi_score import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.urls import include
 
 urlpatterns = [
