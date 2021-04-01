7a1a943f5f25e0b3395a896d8bc125dbc2d07ec5
Darren Fielding <2424957f@student.gla.ac.uk>
2021-04-01 15:14:12 +0100

    Added helper functions

--- hi_score/tests.py
+++ hi_score/tests.py
@@ -2,46 +2,51 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from hi_score.models import UserProfile, Genre, Game, Review
 
+# Helper functions
+def add_user(name):
+    user = User(username = name, password = "SomePassWord#123")
+    user.save()
+    userpro = UserProfile(user = user, rating = 3)
+    userpro.save()
+    return userpro
+
+def add_genre(name):
+    genre = Genre(name = name)
+    genre.save()
+    return genre
+
+def add_game(name, genres):
+    game = Game(name = name, desc = "Some game description")
+    game.save()
+    for genre in genres:
+        game.genres.add(genre)
+    game.save()
+    return game
+
+def add_review(user, game):
+    review = Review(user = user, game = game)
+    review.title = "Some Review Title"
+    review.body = "Some game review"
+    review.rating = 3
+    review.save()
+    return review
+
 class ReviewMethodTests(TestCase):
 
+    def test_slug_line_creation(self):
+        pass
+
     def test_ensure_review_rating_between_one_and_five(self):
-        u1 = User(username = "user1", password = "SomePass#1")
-        u1.save()
-        u2 = User(username = "user2", password = "SomePass#2")
-        u2.save()
-        u3 = User(username = "user3", password = "SomePass#3")
-        u3.save()
-        up1 = UserProfile(user = u1, rating = 1)
-        up1.save()
-        up2 = UserProfile(user = u2, rating = 2)
-        up2.save()
-        up3 = UserProfile(user = u3, rating = 3)
-        up3.save()
+        user1 = add_user("user1")
+        user2 = add_user("user2")
+        user3 = add_user("user3")
 
-        genre = Genre(name = "Genre")
-        genre.save()
-        game = Game(name = "Game", desc = "Hmm")
-        game.save()
-        game.genres.add(genre)
-        game.save()
+        genre = add_genre("Genre")
+        game = add_game("Game", (genre,))
 
-        review1 = Review(user = up1, game = game)
-        review1.title = "Title1"
-        review1.body = "Blah"
-        review1.rating = 0
-        review1.save()
-
-        review2 = Review(user = up2, game = game)
-        review1.title = "Title2"
-        review1.body = "Bleh"
-        review1.rating = 6
-        review1.save()
-
-        review3 = Review(user = up3, game = game)
-        review3.title = "Title3"
-        review3.body = "Bluh"
-        review3.rating = -1
-        review3.save()
+        review1 = add_review(user1, game)
+        review2 = add_review(user2, game)
+        review3 = add_review(user3, game)
 
         self.assertEqual((review1.rating >= 1 and review1.rating <= 5), True)
         self.assertEqual((review2.rating >= 1 and review1.rating <= 5), True)
