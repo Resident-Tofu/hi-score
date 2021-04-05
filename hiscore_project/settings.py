ce7877964b875dfb6b084c486c49de4c9404ac14
Darren Fielding <2424957f@student.gla.ac.uk>
2021-04-05 22:09:24 +0100

    Further edited settings.py for pythonanywhere

--- hiscore_project/settings.py
+++ hiscore_project/settings.py
@@ -25,10 +25,13 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=36fewm(ck5fm-0s+w7)9egvuak&wi13)#w61b5b4@!&s3xff%'
+key = None
+with open("secret.key") as f:
+    key = f.read().strip()
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["residenttofu.pythonanywhere.com"]
 
